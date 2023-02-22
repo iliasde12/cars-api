d504b5a8427a5c967194bf4572bfb66dbe1a35a1
iliasde12 <92632515+iliasde12@users.noreply.github.com>
2023-02-22 14:03:53 +0100

    deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = 'django-insecure-xik@gp*ggywaj10rd$ow1j^4^x&j)a@2lv+w^g7)$^90dc%k5h
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
